1ee9a0b63baf5f36ce493f576301f7b9ae119179
Matthew McClain <mthommcclain@gmail.com>
2025-08-26 08:43:28 -0700

    add unsaved changes warning

--- src/builder.vue
+++ src/builder.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch, onMounted, reactive } from 'vue'
+import { ref, onMounted } from 'vue'
 import * as jsonpatch from 'fast-json-patch'
 
 const props = defineProps({ id: String })
@@ -7,6 +7,7 @@ const props = defineProps({ id: String })
 let formBuilderInstance
 
 const builder = ref(null)
+const unsaved = ref(false)
 const state = await Agent.state(props.id)
 
 if (!state.formData) state.formData = []
@@ -21,20 +22,39 @@ function handleBuilderUpdate() {
       jsonpatch.applyPatch(state.formData, patches)
     }
   }
+  unsaved.value = false
 }
 
 onMounted(async () => {
   formBuilderInstance = await $(builder.value).formBuilder({
     formData: JSON.parse(JSON.stringify(state.formData)),
-    onSave: handleBuilderUpdate
+    onSave: handleBuilderUpdate,
+    onAddField: () => unsaved.value = true,
+    onRemoveField: () => unsaved.value = true,
+    onUpdateField: () => unsaved.value = true,
+    onOpenFieldEdit: () => unsaved.value = true 
   }).promise
+  unsaved.value = false // prevent marked as "unsaved" on init
 })
 
 </script>
 
 <template>
-  <div ref="builder" />
+  <div class="wrapper">
+    <div class="unsaved-warning">
+      <span v-show="unsaved">Your form has unsaved changes</span>
+    </div>
+    <div ref="builder" />
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.unsaved-warning {
+  height: 30px;
+  color: red;
+}
 </style>
