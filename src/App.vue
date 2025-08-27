305226ee98537726e629ad998ea07e9fe5103ab7
Matthew McClain <mthommcclain@gmail.com>
2025-08-27 16:46:23 -0700

    working towards remembering my forms

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
 import { validate as isUUID } from 'uuid'
 import Builder from './builder.vue'
 import Renderer from './renderer.vue'
+import FormSelector from './form-selector.vue'
 import Loading from './loading.vue'
 
-const id = ref(null)
+const activeId = ref(null)
+
+const myLocalForms = reactive({})
+let myKLForms = null
+
 const embedded = Agent.embedded
 const updated = ref(Date.now())
 const ownerIsUser = ref(false)
@@ -15,25 +20,37 @@ const loading = ref(true)
 const FORM_TYPE = "application/json;type=kl-json-form&version=1.0.1"
 
 onMounted(async () => {
+  myKLForms = await Agent.state('my-forms')
+  Object.assign(myLocalForms, myKLForms) 
+
   const { auth: { user } } = await Agent.environment()
   const pathId = window.location.pathname.slice(1)
   validPath.value = isUUID(pathId)
-  if (validPath) {
+  if (validPath.value) {
     const md = await Agent.metadata(pathId)
     ownerIsUser.value = md.owner === user
-    id.value = md.id
+    activeId.value = md.id
   }
   loading.value = false
 })
 
 async function create(formData = [], name = "New Form") {
-  const id = Agent.uuid()
-  const state = await Agent.state(id)
+  const newId = Agent.uuid()
+  const state = await Agent.state(newId)
   state.name = name
   state.formData = formData
-  const md = await Agent.metadata(id)
+  const md = await Agent.metadata(newId)
   md.active_type = FORM_TYPE
+
+  myKLForms[newId] = true
+  myLocalForms[newId] = true
+
   await Agent.synced()
+  window.location = `/${newId}`
+}
+
+function navToFormId(id) {
+  console.log(id)
   window.location = `/${id}`
 }
 
@@ -54,27 +71,32 @@ async function copy(id) {
       v-else-if="validPath === false"
     >
       <button @click="create()">Create Form</button>
+      <FormSelector
+        :forms="myLocalForms"
+        :activeId="activeId"
+        @update="navToFormId"
+      />
     </div>
     <div
       id="container"
-      v-else-if="validPath && id"
+      v-else-if="validPath && activeId"
     >
       <Builder
         v-if="!embedded && ownerIsUser"
-        :id="id"
+        :id="activeId"
         @update="updated = Date.now()"
         @create="create()"
       />
       <div>
         <button
           v-if="!embedded && !ownerIsUser"
-          @click="copy(id)"
+          @click="copy(activeId)"
         >
           Make my own copy to edit
         </button>
         <Renderer
           :key="updated"
-          :id="id"
+          :id="activeId"
         />
       </div>
     </div>
---
+++ src/form-selector.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { reactive, onMounted, defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  forms: {
+    type: Object,
+    required: true
+  },
+  activeId: {
+    type: String,
+    default: null
+  }
+})
+const emit = defineEmits(['update'])
+
+// intial map, id=>id instead of id=>name, before formIdsToNames load
+const initialMap = Object.keys(props.forms)
+  .reduce((acc, id) => {
+      acc[id] = id
+      return acc
+    }, {})
+const formIdsToNames = reactive(initialMap)
+
+onMounted(async () => {
+  const ids = Object.keys(props.forms)
+  const myPromiseArray = ids.map(id => Agent.state(id))
+  const formStates = await Promise.all(myPromiseArray)
+
+  formStates.forEach((st, i) => {
+    const id = ids[i]
+    formIdsToNames[id] = st?.name || id
+  })
+})
+</script>
+
+<template>
+  <div>
+    <select
+		  :value="activeId"
+    	@change="emit('update', $event.target.value)"
+    >
+      <option disabled value="">Select a form...</option>
+      <option
+        v-for="(name, id) in formIdsToNames"
+        :key="id"
+        :value="id"
+      >
+        {{ name }}
+      </option>
+    </select>
+  </div>
+</template>
